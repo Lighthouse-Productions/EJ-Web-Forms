7a960fa1aa71921fff49cc5b4eaf4546a652087c
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-05-04 20:53:09 -0500

    Adjust Whitespace...

--- FormProcessor.php
+++ FormProcessor.php
@@ -15,29 +15,29 @@
         <Zip><![CDATA["?><?php echo $_POST["Zip"]; ?><?php echo"]]></Zip>
         <City><![CDATA["?><?php echo $_POST["City"]; ?><?php echo"]]></City>
         <Email><![CDATA["?><?php echo $_POST["Email"]; ?><?php echo"]]></Email>
-      <Phone><![CDATA["?><?php echo $_POST["Phone"]; ?><?php echo"]]></Phone>
-      <IdCountry><![CDATA[0]]></IdCountry>
-      <State><![CDATA["?><?php echo $_POST["State"]; ?><?php echo"]]></State>
-      <CountryCaption><![CDATA[US]]></CountryCaption>
-    </Address>
-    <ProjectName><![CDATA["?><?php echo $_POST["ProjectName"]; ?><?php echo"]]></ProjectName>
-    <StartDate><![CDATA["?><?php echo $_POST["StartDate"]; ?><?php echo"]]></StartDate>
-    <EndDate><![CDATA["?><?php echo $_POST["EndDate"]; ?><?php echo"]]></EndDate>
-    <Comment><![CDATA["?><?php echo $_POST["Comment"]; ?><?php echo"]]></Comment>
-    <Service.Delivery><![CDATA[False]]></Service.Delivery>
-    <Service.Setup><![CDATA[False]]></Service.Setup>
-    <IdStock><![CDATA[0]]></IdStock>
-    <Service_FullService><![CDATA[False]]></Service_FullService>
-    <IdUserManager><![CDATA[0]]></IdUserManager>
-  </Header>
-  <Body>
-    <Items>
-      <Item>
-        <IdStockType><![CDATA["?><?php echo $_POST["IdStockType"]; ?><?php echo"]]></IdStockType>
-        <Factor><![CDATA["?><?php echo $_POST["Factor"]; ?><?php echo"]]></Factor>
-        <Price><![CDATA["?><?php echo $_POST["Price"]; ?><?php echo"]]></Price>
-      </Item>
-    </Items>
-  </Body>
-</ShopOrder>
+        <Phone><![CDATA["?><?php echo $_POST["Phone"]; ?><?php echo"]]></Phone>
+        <IdCountry><![CDATA[0]]></IdCountry>
+        <State><![CDATA["?><?php echo $_POST["State"]; ?><?php echo"]]></State>
+        <CountryCaption><![CDATA[US]]></CountryCaption>
+      </Address>
+      <ProjectName><![CDATA["?><?php echo $_POST["ProjectName"]; ?><?php echo"]]></ProjectName>
+      <StartDate><![CDATA["?><?php echo $_POST["StartDate"]; ?><?php echo"]]></StartDate>
+      <EndDate><![CDATA["?><?php echo $_POST["EndDate"]; ?><?php echo"]]></EndDate>
+      <Comment><![CDATA["?><?php echo $_POST["Comment"]; ?><?php echo"]]></Comment>
+      <Service.Delivery><![CDATA[False]]></Service.Delivery>
+      <Service.Setup><![CDATA[False]]></Service.Setup>
+      <IdStock><![CDATA[0]]></IdStock>
+      <Service_FullService><![CDATA[False]]></Service_FullService>
+      <IdUserManager><![CDATA[0]]></IdUserManager>
+    </Header>
+    <Body>
+      <Items>
+        <Item>
+          <IdStockType><![CDATA["?><?php echo $_POST["IdStockType"]; ?><?php echo"]]></IdStockType>
+          <Factor><![CDATA["?><?php echo $_POST["Factor"]; ?><?php echo"]]></Factor>
+          <Price><![CDATA["?><?php echo $_POST["Price"]; ?><?php echo"]]></Price>
+        </Item>
+      </Items>
+    </Body>
+  </ShopOrder>
 </root>"?>
