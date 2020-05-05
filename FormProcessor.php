fec59a1e468a02705a0a4af39a44acc6ab18c666
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-05-04 22:42:12 -0500

    Rebuilt XML builder into a single string of XML data before Echo.

--- FormProcessor.php
+++ FormProcessor.php
@@ -1,5 +1,5 @@
-<?php echo
-"<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<?php
+$x='<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <root>
   <ShopOrder Version=\"1.0.0\">
     <Header>
@@ -7,23 +7,51 @@
       <CryptCode><![CDATA[]]></CryptCode>
       <Address>
         <Addressnumber><![CDATA[]]></Addressnumber>
-        <Name1><![CDATA["?><?php echo $_POST["Name1"]; ?><?php echo"]]></Name1>
-        <Firstname><![CDATA["?><?php echo $_POST["Firstname"]; ?><?php echo"]]></Firstname>
-        <Lastname><![CDATA["?><?php echo $_POST["Lastname"]; ?><?php echo"]]></Lastname>
-        <Street><![CDATA["?><?php echo $_POST["Street"]; ?><?php echo"]]></Street>
-        <Line2><![CDATA["?><?php echo $_POST["Line2"]; ?><?php echo"]]></Line2>
-        <Zip><![CDATA["?><?php echo $_POST["Zip"]; ?><?php echo"]]></Zip>
-        <City><![CDATA["?><?php echo $_POST["City"]; ?><?php echo"]]></City>
-        <Email><![CDATA["?><?php echo $_POST["Email"]; ?><?php echo"]]></Email>
-        <Phone><![CDATA["?><?php echo $_POST["Phone"]; ?><?php echo"]]></Phone>
+        <Name1><![CDATA[';
+$x.=$_POST["Name1"];
+$x.=']]></Name1>
+        <Firstname><![CDATA[';
+$x.=$_POST["Firstname"];
+$x.=']]></Firstname>
+        <Lastname><![CDATA[';
+$x.=$_POST["Lastname"];
+$x.=']]></Lastname>
+        <Street><![CDATA[';
+$x.=$_POST["Street"];
+$x.=']]></Street>
+        <Line2><![CDATA[';
+$x.=$_POST["Line2"];
+$x.=']]></Line2>
+        <Zip><![CDATA[';
+$x.=$_POST["Zip"];
+$x.=']]></Zip>
+        <City><![CDATA[';
+$x.=$_POST["City"];
+$x.=']]></City>
+        <Email><![CDATA[';
+$x.=$_POST["Email"];
+$x.=']]></Email>
+        <Phone><![CDATA[';
+$x.=$_POST["Phone"];
+$x.=']]></Phone>
         <IdCountry><![CDATA[0]]></IdCountry>
-        <State><![CDATA["?><?php echo $_POST["State"]; ?><?php echo"]]></State>
+        <State><![CDATA[';
+$x.=$_POST["State"];
+$x.=']]></State>
         <CountryCaption><![CDATA[US]]></CountryCaption>
       </Address>
-      <ProjectName><![CDATA["?><?php echo $_POST["ProjectName"]; ?><?php echo"]]></ProjectName>
-      <StartDate><![CDATA["?><?php echo $_POST["StartDate"]; ?><?php echo"]]></StartDate>
-      <EndDate><![CDATA["?><?php echo $_POST["EndDate"]; ?><?php echo"]]></EndDate>
-      <Comment><![CDATA["?><?php echo $_POST["Comment"]; ?><?php echo"]]></Comment>
+      <ProjectName><![CDATA[';
+$x.=$_POST["ProjectName"];
+$x.=']]></ProjectName>
+      <StartDate><![CDATA[';
+$x.=$_POST["StartDate"];
+$x.=']]></StartDate>
+      <EndDate><![CDATA[';
+$x.=$_POST["EndDate"];
+$x.=']]></EndDate>
+      <Comment><![CDATA[';
+$x.=$_POST["Comment"];
+$x.=']]></Comment>
       <Service.Delivery><![CDATA[False]]></Service.Delivery>
       <Service.Setup><![CDATA[False]]></Service.Setup>
       <IdStock><![CDATA[0]]></IdStock>
@@ -33,11 +61,19 @@
     <Body>
       <Items>
         <Item>
-          <IdStockType><![CDATA["?><?php echo $_POST["IdStockType"]; ?><?php echo"]]></IdStockType>
-          <Factor><![CDATA["?><?php echo $_POST["Factor"]; ?><?php echo"]]></Factor>
-          <Price><![CDATA["?><?php echo $_POST["Price"]; ?><?php echo"]]></Price>
+          <IdStockType><![CDATA[';
+$x.=$_POST["IdStockType"];
+$x.=']]></IdStockType>
+          <Factor><![CDATA[';
+$x.=$_POST["Factor"];
+$x.=']]></Factor>
+          <Price><![CDATA[';
+$x.=$_POST["Price"];
+$x.=']]></Price>
         </Item>
       </Items>
     </Body>
   </ShopOrder>
-</root>"?>
+</root>';
+echo $x;
+?>
