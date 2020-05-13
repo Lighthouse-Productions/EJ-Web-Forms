ee57a4296bae444bf0eb1bb2d17c4b6cdbc55d34
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-05-13 06:20:38 -0500

    Copypaste new function
    From https://webcheatsheet.com/php/send_email_text_html_attachment.php#attachment

--- FormProcessor.php
+++ FormProcessor.php
@@ -80,3 +80,58 @@ $x.=']]></Price>
 file_put_contents("G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm", $x); //Saves X as EWSM XML File - Absolute path specific to server
 echo $x; //To be removed in the future or pushed forward to a crash/confirm landing page.
 ?>
+
+<?php
+//define the receiver of the email
+$to = 'nickw@lhprod.com';
+//define the subject of the email
+$subject = 'Test email with attachment';
+//create a boundary string. It must be unique
+//so we use the MD5 algorithm to generate a random hash
+$random_hash = md5(date('r', time()));
+//define the headers we want passed. Note that they are separated with \r\n
+$headers = "From: webmaster@lhprod.com\r\nReply-To: webmaster@lhprod.com";
+//add boundary string and mime type specification
+$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
+//read the atachment file contents into a string,
+//encode it with MIME base64,
+//and split it into smaller chunks
+$attachment = chunk_split(base64_encode(file_get_contents('ejimport.ewsm')));
+//define the body of the message.
+ob_start(); //Turn on output buffering
+?>
+--PHP-mixed-<?php echo $random_hash; ?>
+Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>"
+
+--PHP-alt-<?php echo $random_hash; ?>
+Content-Type: text/plain; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+Hello World!!!
+This is simple text email message.
+
+--PHP-alt-<?php echo $random_hash; ?>
+Content-Type: text/html; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+<h2>Hello World!</h2>
+<p>This is something with <b>HTML</b> formatting.</p>
+
+--PHP-alt-<?php echo $random_hash; ?>--
+
+--PHP-mixed-<?php echo $random_hash; ?>
+Content-Type: application/zip; name="attachment.zip"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment
+
+<?php echo $attachment; ?>
+--PHP-mixed-<?php echo $random_hash; ?>--
+
+<?php
+//copy current buffer contents into $message variable and delete current output buffer
+$message = ob_get_clean();
+//send the email
+$mail_sent = @mail( $to, $subject, $message, $headers );
+//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
+echo $mail_sent ? "Mail sent" : "Mail failed";
+?>
