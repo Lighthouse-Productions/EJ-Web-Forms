d8b1380ef9f29ab9541a1aaf53117a413bba554d
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-05-17 22:07:07 -0500

    Correctly updates EWSM stored on Server. Attach not working

--- FormProcessor.php
+++ FormProcessor.php
@@ -77,7 +77,7 @@ $x.=']]></Price>
   </ShopOrder>
 </root>';
 //The following line saves the string created above as an EWSM file on the server.
-file_put_contents("G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm", $x); //Saves X as EWSM XML File - Absolute path specific to server
+file_put_contents('G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm', $x); //Saves X as EWSM XML File - Absolute path specific to server
 ?>
 
 <?php
@@ -95,12 +95,12 @@ $headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_h
 //read the atachment file contents into a string,
 //encode it with MIME base64,
 //and split it into smaller chunks
-$attachment = chunk_split(base64_encode(file_get_contents("G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm")));
+$attachment = chunk_split(base64_encode(file_get_contents('G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm')));
 //define the body of the message.
 ob_start(); //Turn on output buffering
-
 //copy current buffer contents into $message variable and delete current output buffer
 $message = $x;
+$message .= $attachment;
 //send the email
 $mail_sent = @mail( $to, $subject, $message, $headers );
 //if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
