6cfcc0688d9957151396fb63e2b709cdd604088c
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-05-22 13:27:17 -0500

    Update FormProcessor.php

--- FormProcessor.php
+++ FormProcessor.php
@@ -79,28 +79,29 @@ $x.=']]></Price>
 //The following line saves the string created above as an EWSM file on the server.
 file_put_contents('G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm', $x); //Saves X as EWSM XML File - Absolute path specific to server
 
-//define the receiver of the email
-$to = 'nickw@lhprod.com';
-//define the subject of the email
+$to = 'nickw@lhprod.com'; //
 $subject = 'New Online Request';
-//create a boundary string. It must be unique
-//so we use the MD5 algorithm to generate a random hash
 $random_hash = md5(date('r', time()));
-//define the headers we want passed. Note that they are separated with \r\n
-$headers = "From: webmaster@lhprod.com\r\nReply-To: webmaster@lhprod.com";
-//add boundary string and mime type specification
-$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
-//read the atachment file contents into a string,
-//encode it with MIME base64,
-//and split it into smaller chunks
 $attachment = chunk_split(base64_encode(file_get_contents('G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm')));
-//define the body of the message.
-ob_start(); //Turn on output buffering
-//copy current buffer contents into $message variable and delete current output buffer
 $message = $x;
-$message .= $attachment;
+$header = "From: webmaster@lhprod.com\r\n";
+$header .= "Reply-To: webmaster@lhprod.com\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-Type: multipart/mixed; boundary=\"".$random_hash."\"\r\n\r\n";
+$header .= "This is a multi-part message in MIME format.\r\n";
+$header .= "--".$random_hash."\r\n";
+$header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+$header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+$header .= $message."\r\n\r\n";
+$header .= "--".$random_hash."\r\n";
+$header .= "Content-Type: application/octet-stream; name=\""'G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm'"\"\r\n"; // use different content types here
+$header .= "Content-Transfer-Encoding: base64\r\n";
+$header .= "Content-Disposition: attachment; filename=\""'G:\PleskVhosts\lhprod.com\httpdocs\ejimport.ewsm'"\"\r\n\r\n";
+$header .= $attachment."\r\n\r\n";
+$header .= "--".$random_hash."--";
+
 //send the email
-$mail_sent = @mail( $to, $subject, $message, $headers );
+$mail_sent = @mail( $to, $subject, $message, $header );
 //if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
 echo $mail_sent ? "Mail sent" : "Mail failed";
 ?>
