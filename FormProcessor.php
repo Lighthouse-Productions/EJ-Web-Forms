f2491bcd2340735a8b869fa8688c312716bf591e
Nicholas Wautier <nwautier@users.noreply.github.com>
2020-06-03 20:10:11 -0500

    Put error message inside of a proper webpage.

--- FormProcessor.php
+++ FormProcessor.php
@@ -93,4 +93,144 @@ $mail_sent = @mail( $to, $subject, "The following information has been recieved 
 //if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
 ?>
 
-<? echo $mail_sent ? "Mail sent" : "Mail failed"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152577184-2">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-152577184-2');
+</script>
+<!-- Start of HubSpot Embed Code -->
+  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7217183.js"></script>
+<!-- End of HubSpot Embed Code -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script src="rollover.js" type="text/javascript"></script>
+<script type="text/javascript" src="fadeslideshow.js">
+
+
+/***********************************************
+* Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
+***********************************************/
+
+</script>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
+	dimensions: [1024, 300], //width/height of gallery in pixels. Should reflect dimensions of largest image
+	imagearray: [
+		["slideshow/slider1.png", "", "", ""],//First Quotes contain image path. Second quotes contain link, Third quotes contain _new. Fourth quotes contain text bottom of image
+		["slideshow/audioslider2.png", "", "", ""],
+		["slideshow/audioslider3.png", "", "", ""],
+		["slideshow/audioslider4.png", "", "", ""],
+		["slideshow/audioslider5.png", "", "", ""],
+		["slideshow/audioslider6.png", "", "", ""] //<--no trailing comma after very last image element!
+		],
+	displaymode: {type:'auto', pause:4000, cycles:0, wraparound:false},
+	persist: false, //remember last viewed slide and recall within same session?
+	fadeduration: 750, //transition duration (milliseconds)
+	descreveal: "ondemand",
+	togglerid: ""
+})
+
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Lighthouse Productions Green Bay WI</title>
+<link rel="stylesheet" type="text/css" href="_css/style.css"/>
+
+<style type="text/css">
+<!--
+h1 {
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+	font-size: 16px;
+	color: #03F;
+}
+
+-->
+    .cse .gsc-control-cse,
+    .gsc-control-cse {
+      background-color:transparent !important;
+    }
+
+</style>
+
+
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryMenuBarVertical.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="wrapper">
+
+<div id="header-cont">
+<div id="header">
+<div id="left_header"><img src="_images/header.png" width="1024" height="110" alt="header image" /></div><!--left header div ending-->
+<div id="right_header">
+<script>
+  (function() {
+    var cx = '017425393430892945240:i-zf-ps4tks';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+    </div> <!--right header div ending-->
+    </div> <!-- header div end -->
+
+<div id="navigation">
+  <ul id="MenuBar1" class="MenuBarHorizontal">
+    <li class="MenuBarHorizontal"><a href="index.html">Home</a></li>
+    <li><a class="MenuBarItemSubmenu" href="audiosystems.html">Audio</a>
+      <ul>
+        <li><a href="pasystems.html">PA Systems</a></li>
+        <li><a href="audioconsoles.html">Consoles</a></li>
+        <li><a href="wireless.html">Wireless</a></li>
+      </ul>
+    </li>
+    <li><a class="MenuBarItemSubmenu" href="lightingsystems.html">Lighting</a>
+      <ul>
+        <li><a href="lightingconsoles.html">Consoles</a></li>
+        <li><a href="movinglights.html">Intelligent Lighting</a></li>
+        <li><a href="conventionallighting.html">Conventional Lighting</a></li>
+        <li><a href="spotlights.html">Spot Lights</a></li>
+        <li><a href="specialfx.html">Special FX</a></li>
+        <li><a href="etctraining.html">Console Training</a></li>
+      </ul>
+  </li>
+        <li><a href="video.html">Video</a></li>
+        <li><a class="MenuBarItemSubmenu" href="roofsystems.html">Roof Systems</a>
+      <ul>
+        <li><a href="_images/Tomcat_Roof.pdf">Tomcat Roof</a></li>
+        <li><a href="_images/Applied_Roof.pdf">Applied Roof</a></li>
+        <li><a href="_images/Blue_Stage.pdf">Blue Stage</a></li>
+      </ul>
+    </li>
+        <li><a class="MenuBarItemSubmenu" href="installs.html">Installations</a>
+    </li>
+        <li><a href="service.html">Service</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+  </ul>
+</div> <!-- navigation div end -->
+</div>  <!-- header-cont div end -->
+
+<div id="fadeshow1"></div> <!-- fadeshow1 div end -->
+<div id="content2">
+<div id="full_content" align="center">
+<? echo $mail_sent ? "Mail sent" : "Mail failed";//PHP Pass Fail ?>
+</div></div></div>
+  </body>
+</html>
